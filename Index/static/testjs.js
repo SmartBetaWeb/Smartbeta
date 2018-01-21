2769d253e9614963175e91ccc4b6a73bc881d2dc
TGY <guangyuantian@yeah.net>
2018-01-21 23:04:22 +0800

    tgy weekend change

---
+++ Index/static/testjs.js
@@ -0,0 +1,5 @@
+  	function changecolor1()
+   	{
+   		x=document.getElementById("test1")  //找到元素
+   		x.style.color="#ff0000";  
+   	}
---
+++ Index/webproj_5.py
@@ -0,0 +1,13 @@
+# 测试JavaScript学习内容
+from flask import Flask
+from flask import request
+from flask_bootstrap import Bootstrap
+from flask import render_template
+
+app = Flask(__name__)
+Bootstrap(app)
+@app.route('/test', methods = ['get'])
+def home_1():
+    return render_template('js_learn1.html')
+if __name__ == '__main__':
+	app.run(host = '127.0.0.1', port = 5000)
